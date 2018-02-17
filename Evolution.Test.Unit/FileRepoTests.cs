b6aa8d2530861b29209a580f26dabd258310f638
Dillon Adams <dillon-adams@utulsa.edu>
2018-02-17 13:37:34 -0600

    adding test to get file contents

--- Evolution.Test.Unit/FileRepoTests.cs
+++ Evolution.Test.Unit/FileRepoTests.cs
@@ -2,6 +2,7 @@
 using Evolution.Exceptions;
 using Evolution.Repo;
 using Moq;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -54,6 +55,23 @@ namespace Evolution.Test.Unit
 
             Assert.Throws<MigrationFileException>(() => repo.CreateMigrationFiles(migrationName));
             Assert.Empty(fileList);
+        }
+
+        [Fact]
+        public void GetMigrationFileContents()
+        {
+            var fileName = "migration1.up.sql";
+            var content = "migration file content";
+
+            var mockContext = new Mock<IFileContext>();
+            mockContext.Setup(c => c.GetMigrationFileContent(It.IsAny<string>())).Returns(content);
+
+            var repo = new FileRepo(mockContext.Object);
+            var contentResult = repo.GetMigrationFileContent(direction);
+
+            Assert.NotNull(contentResult);
+            Assert.NotEmpty(contentResult);
+            Assert.Equal(content, contentResult);
         }
     }
 }
