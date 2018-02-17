a85d9f29f5bddc4c7a0ba35d0e4cf77d362a1cd9
Dillon Adams <dillon-adams@utulsa.edu>
2018-02-17 09:41:41 -0600

    adding unit test to retrieve migrations that have already been executed

---
+++ Evolution.Test.Unit/MigrationRepoTests.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Evolution.Test.Unit
+{
+    public class MigrationRepoTests
+    {
+        [Fact]
+        public void GetExecutedMigrations()
+        {
+            var migrations = new List<IMigration>()
+            {
+                new Migration("Migration1", string.Empty),
+                new Migration("Migration2", string.Empty),
+                new Migration("Migration3", string.Empty)
+            };
+
+            var contextMock = new Mock<IMigrationContext>();
+            contextMock.SetupGet(m => m.Migrations).Returns(migrations);
+            
+            var repo = new MigrationRepo(contextMock.Object);
+            var executedMigrations = repo.GetExecutedMigrations();
+
+            Assert.Equal(migrations.Count, executedMigrations.Count);
+        }
+    }
+}
