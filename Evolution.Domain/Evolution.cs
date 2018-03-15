66a81ab44f26747c196b2076a8557736315de71f
Dillon Adams <dillon-a@hotmail.com>
2018-03-14 19:08:07 -0500

    initial refactor of domain tier

---
+++ Evolution.Domain/Evolution.cs
@@ -0,0 +1,17 @@
+﻿using Evolution.Repo;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Evolution.Domain
+{
+    public class Evolution
+    {
+        private readonly IEvolutionRepo _Repo;
+
+        public Evolution(IEvolutionRepo repo)
+        {
+            _Repo = repo;
+        }
+    }
+}
