6f829313046da49e15b3f6a67f9a898ffc545967
Dillon Adams <dillon-a@hotmail.com>
2018-03-20 06:30:30 -0500

    adding model classes to support testing and enforce clearer delineation between persistence and logic

---
+++ Evolution.Model/Date.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Evolution.Model
+{
+    public class Date : IDate
+    {
+        private DateTime _InstanceDate;
+
+        public Date()
+        {
+            _InstanceDate = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return _InstanceDate.ToString("yyyyMMddHHmmss");
+        }
+    }
+}
---
+++ Evolution.Model/Evolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+
+namespace Evolution.Model
+{
+    public class Evolution
+    {
+        public string FileName { get; }
+
+        public string Name
+        {
+            get
+            {
+                return FileName.Replace(".evo.sql", string.Empty)
+                    .Split('_')
+                    .Last();
+            }
+        }
+
+        public Evolution(IDate date, string fileName)
+        {
+            FileName = fileName;
+        }
+
+        public static string CreateFileName(IDate date, string evolutionName)
+        {
+            return string.Format("{0}_{1}.evo.sql", date.ToString(), evolutionName);
+        }
+    }
+}
