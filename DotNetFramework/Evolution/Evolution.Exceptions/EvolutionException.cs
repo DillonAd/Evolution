3af2ee3d7d91439164617476742adb908b5cdc56
Dillon Adams <dillon-a@hotmail.com>
2018-04-12 12:25:18 -0500

    adding exception project files

---
+++ DotNetFramework/Evolution/Evolution.Exceptions/EvolutionException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Evolution.Exceptions
+{
+    public class EvolutionException : Exception
+    {
+        public EvolutionException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
