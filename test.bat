a0b05097319196bc94fdb548959492e2119f2e4f
Dillon Adams <dillon-a@hotmail.com>
2018-04-12 12:16:34 -0500

    giving up and duplicating the solution for dotnet framework. oracle dlls won't play nice with dotnet standard libraries

--- test.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set containterName=evolutionTest
-docker stop %containterName% 
-docker rm %containterName%
-docker run -v evoOra:/u01/app/oracle/data --name %containterName% --detach=true store/oracle/database-enterprise:12.2.0.1
-dotnet restore
-dotnet build --configuration Release
