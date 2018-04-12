a908ee198f0b52651a8e3a7ae23f930d2707b197
Dillon Adams <dillon-a@hotmail.com>
2018-04-12 12:30:54 -0500

    adding oracle project files

---
+++ DotNetFramework/Evolution/Evolution.Data.Oracle/OracleEvolutionContext.cs
@@ -0,0 +1,25 @@
+﻿using Evolution.Data;
+using Evolution.Data.Entity;
+using System.Data.Entity;
+using System;
+using System.Data.Common;
+
+namespace Evolution.Data.Oracle
+{
+    public class OracleEvolutionContext : DbContext, IEvolutionContext
+    {
+        public virtual DbSet<IEvolution> Evolutions { get; set; }
+
+        public OracleEvolutionContext(string connectionString) : base(connectionString) { }
+
+        public void ExecuteEvolution(string content)
+        {
+            Database.ExecuteSqlCommand(content);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<IEvolution>();
+        }
+    }
+}
