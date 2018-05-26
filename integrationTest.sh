c509d6aecb9c8786f0fcc0cc0b0c0e7b0cdc5199
Dillon Adams <dillon-a@hotmail.com>
2018-05-25 21:37:09 -0500

    updating integration test shell script to output results

--- integrationTest.sh
+++ integrationTest.sh
@@ -26,7 +26,8 @@ echo 'grant dba to c##$oraUser;' | sqlplus "sys/Oradoc_db1@(DESCRIPTION=(ADDRESS
 echo 'grant create session to c##$oraUser;' | sqlplus "sys/Oradoc_db1@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=%oraPort1%))(CONNECT_DATA=(SERVER=dedicated)(SERVICE_NAME=ORCLCDB.localdomain)))" as sysdba
 
 # Run Tests
-dotnet test Evolution.Test\ --filter TestCategory=integration --settings test.runsettings -- TestRunParameters.Parameter.OracleUser="%oraUser%"  TestRunParameters.OraclePassword=%oraPwd% TestRunParameters.OracleInstance=%oraInstance% TestRunParameters.OraclePort=%oraPort1%
+# dotnet test Evolution.Test\ --filter TestCategory=integration --settings test.runsettings -- TestRunParameters.Parameter.OracleUser="%oraUser%"  TestRunParameters.OraclePassword=%oraPwd% TestRunParameters.OracleInstance=%oraInstance% TestRunParameters.OraclePort=%oraPort1%
+dotnet test ./Evolution.Test.Unit/Evolution.Test.Unit.csproj --filter Category=integration --logger "trx;LogFileName=results\tests_integration.xml"
 
 # TearDown Docker container
 docker stop $dbName
