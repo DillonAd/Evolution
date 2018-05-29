02fa77103074ed33eb08fb5320a03f0ccde73d40
Dillon Adams <dillon-a@hotmail.com>
2018-05-29 18:36:13 -0500

    removing unnecessary echo statements inside loop in dockerHealth script

--- dockerHealth.sh
+++ dockerHealth.sh
@@ -4,8 +4,6 @@ echo $(date)
 health=
 until [[ $health == \"healthy\" ]]
 do
-    echo $dbName
 	health=$(docker inspect --format='{{json .State.Health.Status}}' $dbName)
-    echo $health
 done
 echo $(date)
