6a01787bdec78bbcd570d6af7221a754bab82364
Dillon Adams <dillon-a@hotmail.com>
2018-06-04 21:53:21 -0500

    exec options needs

--- Evolution/Options/ExecuteOptions.cs
+++ Evolution/Options/ExecuteOptions.cs
@@ -6,6 +6,7 @@ namespace Evolution.Options
     [Verb("exec")]
     public class ExecuteOptions : DatabaseAuthenticationOptions, IEvolutionTargetable
     {
+        //TODO haz needs exec docs
         [Option("target", HelpText = "Target Evolution", Required = false)]
         public string TargetEvolution { get; set; }
     }
