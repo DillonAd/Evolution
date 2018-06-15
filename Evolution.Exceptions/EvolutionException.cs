96731775fb5db73cfa9e133530f9dc1567c1e816
Dillon Adams <dillon-a@hotmail.com>
2018-06-15 12:35:43 -0500

    updating custom exceptions to shut sonarqube up

--- Evolution.Exceptions/EvolutionException.cs
+++ Evolution.Exceptions/EvolutionException.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Evolution.Exceptions
 {
+    [Serializable]
     public class EvolutionException : Exception
     {
+        public EvolutionException() { }
+
+        public EvolutionException(string message) : base(message) { }
+
         public EvolutionException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected EvolutionException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- Evolution.Exceptions/EvolutionFileException.cs
+++ Evolution.Exceptions/EvolutionFileException.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Evolution.Exceptions
 {
+    [Serializable]
     public class EvolutionFileException : Exception
     {
+        public EvolutionFileException() { }
+
         public EvolutionFileException(string message) : base(message) { }
+
+        public EvolutionFileException(string message, Exception innerException) : base(message, innerException) { }
+
+        protected EvolutionFileException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
