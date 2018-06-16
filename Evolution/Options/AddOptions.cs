ea0a699ea7d385a5ba0301da0adf3911a9b6bee8
Dillon Adams <dillon-a@hotmail.com>
2018-06-15 21:32:43 -0500

    add command helptext

--- Evolution/Options/AddOptions.cs
+++ Evolution/Options/AddOptions.cs
@@ -3,13 +3,12 @@ using Evolution.Model;
 
 namespace Evolution.Options
 {
-    [Verb("add")]
+    [Verb("add", HelpText = "Creates an Evolution file from a specified source file.")]
     public class AddOptions : DatabaseAuthenticationOptions, IEvolutionCreatable
     {
         [Option("source", HelpText = "Source file to create Evolution")]
         public string SourceFile { get; set; }
 
-        //TODO Add HelpText for AddOptions
         [Option("target", HelpText = "Name of the evolution to create.")]
         public string TargetEvolution { get; set; }
     }
