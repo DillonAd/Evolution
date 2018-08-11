3c75db357496f339c0af45bdfeea00875b1d5dd4
Dillon <dillon-a@hotmail.com>
2018-08-11 11:36:03 -0500

    updating runOracle script

--- Setup/runOracle.sh
+++ Setup/runOracle.sh
@@ -1,2 +1,2 @@
 docker run -d --name evolution --rm -p 6666:1521 -p 6667:5500 -e ORACLE_SID=evolution store/oracle/database-enterprise:12.2.0.1-slim
-sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql    
+sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql
