e323503f03e727750311c18005933a10be28359e
Dillon <dillon-a@hotmail.com>
2018-08-15 20:38:19 -0500

    adding dockerHealth.sh script to the oracle setup

--- Setup/runOracle.sh
+++ Setup/runOracle.sh
@@ -1,2 +1,12 @@
 docker run -d --name evolution --rm -p 6666:1521 -p 6667:5500 -e ORACLE_SID=evolution store/oracle/database-enterprise:12.2.0.1-slim
+
+dbName=$1
+echo $(date)
+health=
+until [[ $health == \"healthy\" ]]
+do
+	health=$(docker inspect --format='{{json .State.Health.Status}}' $dbName)
+done
+echo $(date)
+
 sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql
