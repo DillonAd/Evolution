dbf2db0449fde64d70d999f78983c07c04afb77b
Dillon <dillon-a@hotmail.com>
2018-08-16 09:09:08 -0500

    using same db environment as build

--- Setup/runOracle.sh
+++ Setup/runOracle.sh
@@ -1,12 +1,3 @@
-docker run -d --name evolution --rm -p 6666:1521 -p 6667:5500 -e ORACLE_SID=evolution store/oracle/database-enterprise:12.2.0.1-slim
-
-dbName=$1
-echo $(date)
-health=
-until [[ $health == \"healthy\" ]]
-do
-	health=$(docker inspect --format='{{json .State.Health.Status}}' $dbName)
-done
-echo $(date)
-
-sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql
+docker run -d --name evolution --rm -p 6666:1521 -p 6667:5500 -e ORACLE_SID=evolution store/oracle/database-enterprise:12.2.0.1
+docker cp ./SetupOracle.sql evolution:SetupOracle.sql
+docker exec evolution bash -c source /home/oracle/.bashrc; sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql; exit \$?
