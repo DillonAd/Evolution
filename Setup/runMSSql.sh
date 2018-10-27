89c2e470c05f159f25050776a0a4d840335654e7
dillon <28228453+DillonAd@users.noreply.github.com>
2018-10-27 11:57:07 -0500

    Fixing issues with script not running due to bash interpretations of 'strong' passwords

--- Setup/runMSSql.sh
+++ Setup/runMSSql.sh
@@ -1,3 +1,3 @@
-docker run -d --name evolution-sql --rm -e ACCEPT_EULA=Y -e MSSQL_PID=Express -e SA_PASSWORD=YourStrong!Passw0rd microsoft/mssql-server-linux
+docker run -d --name evolution-sql --rm -e ACCEPT_EULA=Y -e MSSQL_PID=Express -e 'SA_PASSWORD=YourStrong!Passw0rd' microsoft/mssql-server-linux
 docker cp ./SetupMSSql.sql evolution-sql:SetupMSSql.sql
-docker exec evolution-sql /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U SA -P YourStrong!Passw0rd -i SetupMSSql.sql
+docker exec evolution-sql /opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U SA -P 'YourStrong!Passw0rd' -i ./SetupMSSql.sql
