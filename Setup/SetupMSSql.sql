a94e4f9e571bb3069872e1f005f103c3734b5ec5
dillon <28228453+DillonAd@users.noreply.github.com>
2018-10-27 20:20:47 -0500

    Cleaning out the last of the Oracle remnants from Sql Server test scripts

--- Setup/SetupMSSql.sql
+++ Setup/SetupMSSql.sql
@@ -2,11 +2,11 @@ USE master
 GO
 IF(DB_ID(N'evolutionDB') IS NULL) CREATE DATABASE evolutionDB;
 GO
-CREATE LOGIN appUser WITH PASSWORD = 'appPassword1';  
+CREATE LOGIN appUser WITH PASSWORD = 'appPassword1';
 GO
 USE evolutionDB;
 GO
 CREATE USER appUser FOR LOGIN appUser;
 GO
 EXEC sp_addrolemember 'db_owner', 'appUser'
-go
+GO
