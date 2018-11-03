f8375744e86d15eae21452e6c8c3ae63c5030f13
dillon <28228453+DillonAd@users.noreply.github.com>
2018-11-03 15:00:40 -0500

    Moving MsSql setup to subdirectory

---
+++ Evolution.Test.Integration/Setup/MsSql/SetupMSSql.sql
@@ -0,0 +1,12 @@
+USE master
+GO
+IF(DB_ID(N'evolutionDB') IS NULL) CREATE DATABASE evolutionDB;
+GO
+CREATE LOGIN appUser WITH PASSWORD = 'appPassword1';
+GO
+USE evolutionDB;
+GO
+CREATE USER appUser FOR LOGIN appUser;
+GO
+EXEC sp_addrolemember 'db_owner', 'appUser'
+GO
