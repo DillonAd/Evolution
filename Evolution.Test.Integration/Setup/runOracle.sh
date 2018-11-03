473fca49d80ba535410ff636b4fb75c2518fff5f
dillon <28228453+DillonAd@users.noreply.github.com>
2018-11-03 14:56:46 -0500

    Moving Oracle scripts to subdirectory and fixing the run script

--- Evolution.Test.Integration/Setup/runOracle.sh
+++
@@ -1,3 +0,0 @@
-docker run -d --name evolution --rm -p 6666:1521 -p 6667:5500 -e ORACLE_SID=evolution store/oracle/database-enterprise:12.2.0.1
-docker cp ./SetupOracle.sql evolution:SetupOracle.sql
-docker exec evolution bash -c source /home/oracle/.bashrc; sqlplus sys/Oradoc_db1@localhost:6666/ORCLCDB.localdomain as sysdba @SetupOracle.sql; exit \$?
