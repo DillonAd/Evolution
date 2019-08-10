479c136a1c857a5695623ac96f170c422c575083
dillon <28228453+DillonAd@users.noreply.github.com>
2019-08-09 20:57:02 -0500

    Cleaning up the Context Factory

--- Evolution/DbContextFactory.cs
+++ Evolution/DbContextFactory.cs
@@ -10,39 +10,42 @@ namespace Evolution
     {
         public static IEvolutionContext CreateContext(IDatabaseAuthenticationOptions dbAuthOptions)
         {
+            IConnectionStringBuilder connectionStringBuilder;
+            IEvolutionContext context;
+
             switch (dbAuthOptions.Type)
             {
                 case DatabaseTypes.Oracle:
+                    connectionStringBuilder = new OracleConnectionBuilder()
                     {
-                        var connectionStringBuilder = new OracleConnectionBuilder()
-                        {
-                            UserName = dbAuthOptions.User,
-                            Password = dbAuthOptions.Password,
-                            Server = dbAuthOptions.Server,
-                            Instance = dbAuthOptions.Instance,
-                            Port = dbAuthOptions.Port
-                        };
+                        UserName = dbAuthOptions.User,
+                        Password = dbAuthOptions.Password,
+                        Server = dbAuthOptions.Server,
+                        Instance = dbAuthOptions.Instance,
+                        Port = dbAuthOptions.Port
+                    };
 
-                        return new OracleEvolutionContext(connectionStringBuilder.CreateConnectionString());
-                    }
+                    context = new OracleEvolutionContext(connectionStringBuilder.CreateConnectionString());
+                    break;
 
                 case DatabaseTypes.MSSql:
+                    connectionStringBuilder = new SqlClientConnectionBuilder()
                     {
-                        var connectionStringBuilder = new SqlClientConnectionBuilder()
-                        {
-                            UserName = dbAuthOptions.User,
-                            Password = dbAuthOptions.Password,
-                            Server = dbAuthOptions.Server,
-                            Instance = dbAuthOptions.Instance,
-                            Port = dbAuthOptions.Port
-                        };
+                        UserName = dbAuthOptions.User,
+                        Password = dbAuthOptions.Password,
+                        Server = dbAuthOptions.Server,
+                        Instance = dbAuthOptions.Instance,
+                        Port = dbAuthOptions.Port
+                    };
 
-                        return new SqlClientEvolutionContext(connectionStringBuilder.CreateConnectionString());
-                    }
+                    context = new SqlClientEvolutionContext(connectionStringBuilder.CreateConnectionString());
+                    break;
 
                 default:
                     throw new NotSupportedException("The type of database provided is not supported");
             }
+
+            return context;
         }
     }
 }
