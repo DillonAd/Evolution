9f247a5d7722642881eb8c1fc1264f0056e66871
dillon <28228453+DillonAd@users.noreply.github.com>
2019-08-09 20:50:08 -0500

    Streamlining Program creation

--- Evolution/Program.cs
+++ Evolution/Program.cs
@@ -33,11 +33,47 @@ namespace Evolution
         
         private static IEvolutionLogic GetApplication(IDatabaseAuthenticationOptions dbAuthOptions)
         {
+            PopulateMissingConfigValues(ref dbAuthOptions);
             var context = DbContextFactory.CreateContext(dbAuthOptions);
             var repo = new EvolutionRepo(context);
             var fileSystem = new FileContext();
             var fileSystemRepo = new FileRepo(fileSystem);
             return new EvolutionLogic(repo, fileSystemRepo);
+        }
+
+        private static void PopulateMissingConfigValues<TOption>(ref TOption options)
+            where TOption : class
+        {
+            var newOptions = options;
+            string configValue;
+            object parseValue;
+
+            foreach(var property in options.GetType().GetProperties())
+            {
+                configValue = _configOptions.GetConfigurationValue(property.Name);
+
+                if(string.IsNullOrWhiteSpace(configValue))
+                {
+                    continue;
+                }
+
+                //Don't override values that are passed in via CLI
+                if(property.GetValue(options)?.ToString() == configValue)
+                {
+                    continue;
+                }
+
+                if(property.PropertyType.IsEnum)
+                {
+                    parseValue = Enum.Parse(property.PropertyType, configValue) as Enum;
+                }
+                else
+                {
+                    parseValue = Convert.ChangeType(configValue, property.PropertyType);
+                }
+
+                property.SetValue(options, parseValue);
+            }
         }
     }
 }
